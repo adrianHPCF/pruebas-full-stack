032b15f909bca93bf502fd5000d8d53420eefc4e
adrianHPCF <126709464+adrianHPCF@users.noreply.github.com>
2023-09-20 20:14:17 +0200

    hacemos un pull

--- src/sourcetree/nombre.java
+++ src/sourcetree/nombre.java
@@ -3,8 +3,7 @@ package sourcetree;
 public class nombre {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		system.out.println("barabarabara bereberebere")
 	}
 
 }
